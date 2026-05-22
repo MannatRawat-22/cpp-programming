2204d35dfdcb6ac4969c6a8bfb8e57356818cdae
mannatr2210-pixel <mannatr2210@gmail.com>
2026-05-22 21:05:08 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    switch(op) {
+        case '+':
+            cout << "Result = " << a + b;
+            break;
+
+        case '-':
+            cout << "Result = " << a - b;
+            break;
+
+        case '*':
+            cout << "Result = " << a * b;
+            break;
+
+        case '/':
+            cout << "Result = " << a / b;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
